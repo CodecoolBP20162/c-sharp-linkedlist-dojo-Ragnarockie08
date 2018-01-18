c9028cb8517b10ae4919b5b8c1212269a0b0c2ba
ragnarockie08 <ragnarockie08@gmail.com>
2018-01-18 11:45:13 +0100

    project moved from other directory

---
+++ src/main/java/Program.java
@@ -0,0 +1,16 @@
+import linked_list.singleList.SingleLinkedList;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+      SingleLinkedList<Integer> list = new SingleLinkedList();
+
+      list.add(1);
+      list.add(2);
+      list.add(3);
+      list.add(4);
+      System.out.println(list);
+
+    }
+}
